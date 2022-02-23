8ce17232cb9471c6afa48540c198473959f0b4bb
Ariel Berkowicz <berkowicz.ari@gmail.com>
2022-02-22 23:39:55 -0500

    Changed the ports to 8000

--- testing_data.py
+++ testing_data.py
@@ -44,5 +44,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
-    application.run()
-
+    application.run(host="localhost", port=8000, debug=True)
