55aa17f4b1eb705e34b4126f35eb8db1b811ffdb
Ariel Berkowicz <berkowicz.ari@gmail.com>
2022-03-15 20:35:49 -0400

    Added an error list to be checked later for testing

--- device_module.py
+++ device_module.py
@@ -3,6 +3,7 @@
 import json
 import datetime
 
+
 def parse_json(filename):
     """
     Parses a json file and returns a list of dictionaries
@@ -18,6 +19,7 @@ def parse_json(filename):
         # Return the data
         return data
 
+
 # Log the data to the file in a folder
 def log_to_folder(data):
     """
@@ -29,10 +31,40 @@ def log_to_folder(data):
         fp.write(json.dumps(data) + '\n')
         pass
 
+
+def errors():
+    """
+    Returns a list of errors
+    """
+    errors = []
+    # Check if the data is a dictionary
+    if not isinstance(data, dict):
+        errors.append('The data is not a dictionary')
+    # Check if the data has the correct keys
+    if not all(
+            key in data
+            for key in ['timestamp', 'temperature', 'humidity', 'pressure']):
+        errors.append(
+            'The data is missing the following keys: timestamp, temperature, humidity, pressure'
+        )
+    # Check if the data has the correct values
+    if not all(
+            isinstance(data[key], (int, float))
+            for key in ['temperature', 'humidity', 'pressure']):
+        errors.append(
+            'The data has the following values that are not numbers: temperature, humidity, pressure'
+        )
+    # Check if the data has the correct values
+    if not isinstance(data['timestamp'], str):
+        errors.append(
+            'The data has the following values that are not strings: timestamp'
+        )
+    # Return the errors list
+    return errors
+
+
 if __name__ == '__main__':
     # Get the data from the json file
     data = parse_json('data.json')
     # Log the data to the file
     log_to_folder(data)
-
-    
